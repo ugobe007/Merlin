bf1f4ff04ec9a3b04e9f20fc226608b3240c6c90
ugobe007 <ugobe07@gmail.com>
2025-10-18 13:57:54 -0700

    chore(types): add shims for untyped modules (mammoth, DocumentUploadModal)

---
+++ src/types/shims.d.ts
@@ -0,0 +1,4 @@
+// Minimal shims for modules without type declarations to unblock the TypeScript compiler.
+// Add more `declare module '...'` lines here for other untyped packages or local modules.
+declare module 'mammoth'
+declare module '../upload/DocumentUploadModal'
